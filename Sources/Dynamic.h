6f95e67b908eaaed06d015aa688c64e6a2813a9a
Mark Bourke <markkbourke@gmail.com>
2022-01-26 14:39:27 +0000

    Initial refactor

---
+++ Sources/Dynamic.h
@@ -0,0 +1,18 @@
+//
+//  Dynamic.h
+//  Dynamic
+//
+//  Created by Mark Bourke on 26/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Dynamic.
+FOUNDATION_EXPORT double DynamicVersionNumber;
+
+//! Project version string for Dynamic.
+FOUNDATION_EXPORT const unsigned char DynamicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dynamic/PublicHeader.h>
+
+
---
+++ Sources/Dynamic/Dynamic.swift
@@ -0,0 +1,8 @@
+//
+//  Dynamic.swift
+//  Resolver
+//
+//  Created by Mark Bourke on 26/01/2022.
+//
+
+import Foundation
